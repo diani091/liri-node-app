66527d5171f32d0ed582ea1f12269f830e8ba782
Diani <diana.mty@fabcafe.com>
2018-06-27 19:47:06 -0500

    hw details

---
+++ key.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key:"KxEfXhMM7gWM1QpfLkSb0l0a2",
+  consumer_secret:"j3lxndtOsKytAFQAe6v4q54Yz3InL6HaHMar76Fb9b4JVf4Lwq",
+  access_token_key:"1011702048675516418-7BhHXAjnEDDEasEMsg4DJvzS1x8zgf",
+  access_token_secret:"smVi8nETzr3y17Ds9Au2BZzoKcf35oc7BycFtBn7NKHgB"
+};
+
+exports.spotify = {
+  id:"60387f3b287644948c6c62b6d8b491bc",
+  secret:"d265aec0781746fcbb3af0f3ae70b8e6",
+};
